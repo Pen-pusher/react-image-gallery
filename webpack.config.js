f5d3c7600fc65e01b606e3728838fae12df0de45
Yow-Long Lee <yowlonglee@gmail.com>
2017-12-26 14:09:21 +0800

    Update configuration

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const WebpackNotifierPlugin = require('webpack-notifier');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/build'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: '[name]--[local]--[hash:base64:8]'
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          {
+            loader: 'csv-loader'
+          }
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          {
+            loader: 'xml-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new WebpackNotifierPlugin({
+      alwaysNotify: true
+    })
+  ]
+};
