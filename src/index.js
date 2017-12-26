fabc4e5100f6d8af424ae18f5392e4451c3f6664
Yow-Long Lee <yowlonglee@gmail.com>
2017-12-26 22:39:48 +0800

    Add base.css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
 import 'normalize.css';
+import './base.css';
 import App from './components/app';
 
 const fetchUrl = 'https://api.unsplash.com/collections/';
 const id = '2ebf57611c3566052a250a60c74cd822ac81e4198686d9dc5607a532508937f4';
-const query = `${fetchUrl}?client_id=${id}`;
-const promise = fetch(query);
+const myHeaders = new Headers();
+myHeaders.append('Authorization', `Client-ID ${id}`);
+const init = {
+  method: 'GET',
+  headers: myHeaders
+};
+const promise = fetch(fetchUrl, init);
 
 promise
   .then(response => response.json())
