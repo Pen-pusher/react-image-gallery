dcd7485bb7ef12060e83346b8242bcc456a72375
Yow-Long Lee <yowlonglee@gmail.com>
2017-12-26 14:30:11 +0800

    Add ListGroup

--- src/components/app.js
+++ src/components/app.js
@@ -2,19 +2,23 @@ import React from 'react';
 import { AppBar, Layout } from 'react-toolbox';
 import appStyle from './app.css';
 
+const ListGroup = (props) => {
+  const items = props.listItems;
+  const content = items.map(item => (
+    <li key={item.id}>
+      <span>
+        {item.title}
+      </span>
+    </li>));
+  return <ul>{content}</ul>;
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.data = props.data;
   }
   render() {
-    const items = this.data;
-    const listItems = items.map(item => (
-      <li key={item.id}>
-        <span>
-          {item.title}
-        </span>
-      </li>));
     return (
       <div>
         <Layout>
@@ -22,9 +26,7 @@ class App extends React.Component {
           <div className={appStyle.content}>
             <div>相簿</div>
             <div>
-              <ul>
-                {listItems}
-              </ul>
+              <ListGroup listItems={this.data} />
             </div>
           </div>
         </Layout>
