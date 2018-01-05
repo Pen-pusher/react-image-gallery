56ffe15e8c7d74b90c80152959782e8dca02aa8c
Yow-Long Lee <yowlonglee@gmail.com>
2018-01-05 16:32:02 +0800

    Add Slides component

--- src/components/Lightbox.js
+++ src/components/Lightbox.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import { Layout, Panel } from 'react-toolbox';
+import FontIcon from 'react-toolbox/lib/font_icon';
+import Slides from './Slides';
+// import { Layout, Panel } from 'react-toolbox';
 import Style from './Lightbox.css';
 
 class Lightbox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: {
+        index: props.index,
+        description: props.slides[props.index].description,
+        link: props.slides[props.index].links.html,
+        download: props.slides[props.index].links.download
+      }
+    };
+  }
 
   render() {
     return (
       <div className={Style.lightbox}>
-        <div>
+      <Slides />
+        <div className={Style['tool-bar']}>
           <div>layout test</div>
         </div>
+        <div className={Style['description-bar']}>
+          <div>{this.state.active.description}</div>
+        </div>
+        <div className={Style.prev}>
+          <FontIcon value="chevron_left" />
+        </div>
+        <div className={Style.next}>
+          <FontIcon value="chevron_right" />
+        </div>
       </div>
-    )
+    );
   }
 }
 
---
+++ src/components/Slides.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Slides extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div>Slides</div>;
+  }
+}
+
+export default Slides;
