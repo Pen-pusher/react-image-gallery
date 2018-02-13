c8701f435159e5a3e063c07fa9fd291cecaa33c8
Yow-Long Lee <yowlonglee@gmail.com>
2018-02-13 17:26:00 +0800

    Add a second id

--- src/Fetch.js
+++ src/Fetch.js
@@ -1,6 +1,7 @@
 const Fetch = (pathname) => {
   const fetchUrl = `https://api.unsplash.com/${pathname}`;
-  const id = '2ebf57611c3566052a250a60c74cd822ac81e4198686d9dc5607a532508937f4';
+  // const id = '2ebf57611c3566052a250a60c74cd822ac81e4198686d9dc5607a532508937f4';
+  const id = '7b731361acf025d0a69683c791e73441ca2b21871aec8a4a6e378ebb76cdba06';
   const myHeaders = new Headers();
   myHeaders.append('Authorization', `Client-ID ${id}`);
   const init = {
