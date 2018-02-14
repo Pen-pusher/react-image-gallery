ae179bfc24344b3df78a15e7f458d0ce2069e952
Yow-Long Lee <yowlonglee@gmail.com>
2018-02-14 11:31:23 +0800

    Update bundled file extension

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     './src/index.js'
   ],
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.min.js',
     path: path.resolve(__dirname, 'build'),
     publicPath: '/build'
   },
